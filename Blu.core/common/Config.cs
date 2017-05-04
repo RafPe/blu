20dfc3bfaa1dda67da1f51763be14d979e178bc1
RafPe <rafal@pieniazek.nl>
2017-05-04 17:07:55 +0200

    Adds core library

---
+++ Blu.core/common/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Blu.core.common
+{
+    public static class Config
+    {
+        public const string ServiceName = "BluService";
+        public const string ExecuteCommand = "exec";
+        public const string CreateRunspaceCommand = "create-runspace";
+        public const string DisposeRunspaceCommand = "dispose-runspace";
+        public const string MagicSplitString = "-:#:#:#:-";
+        public const string ShellName = "BluShell";
+        public const string PipeName = "BluPowerShell";
+        public static string SeparatorLine = Environment.NewLine +
+                                             "------------------------------------------" +
+                                             Environment.NewLine;
+
+        public static string TempPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location ?? ""), "temp-scripts");
+        public const string RunspaceExecutionDone = "execution-of-script-completed-@341!";
+    }
+}
